c70affc025cb1bacf12e478a38c9ed854ff40f54
Mehmet Paçal <51230161+mettleshade@users.noreply.github.com>
2022-04-09 23:22:07 +0300

    .

---
+++ Hızlı Mesaj Gönderme/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿Partial Public Class Form1
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    ''' <summary>
+    ''' Required designer variable.
+    ''' </summary>
+    Private components As System.ComponentModel.IContainer = Nothing
+
+    ''' <summary>
+    ''' Clean up any resources being used.
+    ''' </summary>
+    ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso (components IsNot Nothing) Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+#Region "Windows Form Designer generated code"
+
+    ''' <summary>
+    ''' Required method for Designer support - do not modify
+    ''' the contents of this method with the code editor.
+    ''' </summary>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.FormAssistant1 = New DevExpress.XtraBars.FormAssistant()
+        Me.DefaultLookAndFeel1 = New DevExpress.LookAndFeel.DefaultLookAndFeel(Me.components)
+        Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+        Me.SimpleButton2 = New DevExpress.XtraEditors.SimpleButton()
+        Me.MemoEdit1 = New DevExpress.XtraEditors.TextEdit()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.TextEdit1 = New DevExpress.XtraEditors.ComboBoxEdit()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        Me.LabelControl3 = New DevExpress.XtraEditors.LabelControl()
+        Me.CheckEdit1 = New DevExpress.XtraEditors.CheckEdit()
+        CType(Me.MemoEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CheckEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DefaultLookAndFeel1
+        '
+        Me.DefaultLookAndFeel1.LookAndFeel.SkinName = "Office 2016 Dark"
+        '
+        'SimpleButton1
+        '
+        Me.SimpleButton1.ImageOptions.Image = CType(resources.GetObject("SimpleButton1.ImageOptions.Image"), System.Drawing.Image)
+        Me.SimpleButton1.Location = New System.Drawing.Point(12, 91)
+        Me.SimpleButton1.Name = "SimpleButton1"
+        Me.SimpleButton1.Size = New System.Drawing.Size(123, 34)
+        Me.SimpleButton1.TabIndex = 0
+        Me.SimpleButton1.Text = "Başlat (F5)"
+        '
+        'SimpleButton2
+        '
+        Me.SimpleButton2.ImageOptions.Image = CType(resources.GetObject("SimpleButton2.ImageOptions.Image"), System.Drawing.Image)
+        Me.SimpleButton2.Location = New System.Drawing.Point(141, 91)
+        Me.SimpleButton2.Name = "SimpleButton2"
+        Me.SimpleButton2.Size = New System.Drawing.Size(128, 34)
+        Me.SimpleButton2.TabIndex = 1
+        Me.SimpleButton2.Text = "Durdur (F6)"
+        '
+        'MemoEdit1
+        '
+        Me.MemoEdit1.Location = New System.Drawing.Point(12, 36)
+        Me.MemoEdit1.Name = "MemoEdit1"
+        Me.MemoEdit1.Size = New System.Drawing.Size(188, 20)
+        Me.MemoEdit1.TabIndex = 2
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Appearance.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.LabelControl1.Appearance.ForeColor = System.Drawing.Color.Black
+        Me.LabelControl1.Appearance.Options.UseFont = True
+        Me.LabelControl1.Appearance.Options.UseForeColor = True
+        Me.LabelControl1.Location = New System.Drawing.Point(12, 14)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(145, 16)
+        Me.LabelControl1.TabIndex = 3
+        Me.LabelControl1.Text = "Harf,Kelime veya Cümle :"
+        '
+        'TextEdit1
+        '
+        Me.TextEdit1.EditValue = ""
+        Me.TextEdit1.Location = New System.Drawing.Point(206, 36)
+        Me.TextEdit1.Name = "TextEdit1"
+        Me.TextEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.TextEdit1.Properties.Items.AddRange(New Object() {"Çok Yavaş", "Yavaş", "Orta", "Hızlı", "Çok Hızlı", "Aşırı Hızlı"})
+        Me.TextEdit1.Properties.TextEditStyle = DevExpress.XtraEditors.Controls.TextEditStyles.DisableTextEditor
+        Me.TextEdit1.Size = New System.Drawing.Size(63, 20)
+        Me.TextEdit1.TabIndex = 4
+        '
+        'Timer1
+        '
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Appearance.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.LabelControl2.Appearance.ForeColor = System.Drawing.Color.Black
+        Me.LabelControl2.Appearance.Options.UseFont = True
+        Me.LabelControl2.Appearance.Options.UseForeColor = True
+        Me.LabelControl2.Location = New System.Drawing.Point(206, 14)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(26, 16)
+        Me.LabelControl2.TabIndex = 5
+        Me.LabelControl2.Text = "Hız :"
+        '
+        'LabelControl3
+        '
+        Me.LabelControl3.Appearance.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.LabelControl3.Appearance.Options.UseForeColor = True
+        Me.LabelControl3.Location = New System.Drawing.Point(13, 131)
+        Me.LabelControl3.Name = "LabelControl3"
+        Me.LabelControl3.Size = New System.Drawing.Size(201, 13)
+        Me.LabelControl3.TabIndex = 6
+        Me.LabelControl3.Text = "Mettless Dizayn - Yazı Yazdırma <FİNAL> "
+        '
+        'CheckEdit1
+        '
+        Me.CheckEdit1.Location = New System.Drawing.Point(13, 63)
+        Me.CheckEdit1.Name = "CheckEdit1"
+        Me.CheckEdit1.Properties.Appearance.ForeColor = System.Drawing.Color.Gray
+        Me.CheckEdit1.Properties.Appearance.Options.UseForeColor = True
+        Me.CheckEdit1.Properties.Caption = "Her yazmada Enter'e basılsın."
+        Me.CheckEdit1.Size = New System.Drawing.Size(256, 19)
+        Me.CheckEdit1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.Appearance.BackColor = System.Drawing.Color.White
+        Me.Appearance.Options.UseBackColor = True
+        Me.ClientSize = New System.Drawing.Size(281, 152)
+        Me.Controls.Add(Me.CheckEdit1)
+        Me.Controls.Add(Me.LabelControl3)
+        Me.Controls.Add(Me.LabelControl2)
+        Me.Controls.Add(Me.LabelControl1)
+        Me.Controls.Add(Me.SimpleButton2)
+        Me.Controls.Add(Me.SimpleButton1)
+        Me.Controls.Add(Me.MemoEdit1)
+        Me.Controls.Add(Me.TextEdit1)
+        Me.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Shadow
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Otomatik Yazı Yazdırma"
+        CType(Me.MemoEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CheckEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents FormAssistant1 As DevExpress.XtraBars.FormAssistant
+    Friend WithEvents DefaultLookAndFeel1 As DevExpress.LookAndFeel.DefaultLookAndFeel
+    Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents SimpleButton2 As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents MemoEdit1 As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents TextEdit1 As DevExpress.XtraEditors.ComboBoxEdit
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents LabelControl3 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents CheckEdit1 As DevExpress.XtraEditors.CheckEdit
+
+#End Region
+
+End Class
---
+++ Hızlı Mesaj Gönderme/Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+
+
+Partial Public Class Form1
+    Shared Sub New()
+        DevExpress.UserSkins.BonusSkins.Register()
+        DevExpress.Skins.SkinManager.EnableFormSkins()
+    End Sub
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    Private Sub SimpleButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SimpleButton1.Click
+        If TextEdit1.Text = "" Or MemoEdit1.Text = "" Then
+            MsgBox("Lütfen hız seçin veya yazılacak kelime,cümle,harf Girin.!", MsgBoxStyle.Critical, "Dikkat !")
+        Else
+            If TextEdit1.Text = "Çok Yavaş" Then
+                Timer1.Interval = 700
+                Timer1.Start()
+
+            End If
+            If TextEdit1.Text = "Yavaş" Then
+                Timer1.Interval = 500
+                Timer1.Start()
+            End If
+            If TextEdit1.Text = "Orta" Then
+                Timer1.Interval = 450
+                Timer1.Start()
+            End If
+            If TextEdit1.Text = "Hızlı" Then
+                Timer1.Interval = 300
+                Timer1.Start()
+            End If
+            If TextEdit1.Text = "Çok Hızlı" Then
+                Timer1.Interval = 200
+                Timer1.Start()
+            End If
+            If TextEdit1.Text = "Aşırı Hızlı" Then
+                Timer1.Interval = 100
+                Timer1.Start()
+            End If
+        End If
+        
+    End Sub
+
+    Private Sub SimpleButton2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SimpleButton2.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+        If (e.KeyCode = Keys.F5) Then
+            If TextEdit1.Text = "" Or MemoEdit1.Text = "" Then
+                MsgBox("Lütfen hız seçin veya yazılacak kelime,cümle,harf Girin.!", MsgBoxStyle.Critical, "Dikkat !")
+            Else
+                If TextEdit1.Text = "Çok Yavaş" Then
+                    Timer1.Interval = 700
+                    Timer1.Start()
+
+                End If
+                If TextEdit1.Text = "Yavaş" Then
+                    Timer1.Interval = 500
+                    Timer1.Start()
+                End If
+                If TextEdit1.Text = "Orta" Then
+                    Timer1.Interval = 450
+                    Timer1.Start()
+                End If
+                If TextEdit1.Text = "Hızlı" Then
+                    Timer1.Interval = 300
+                    Timer1.Start()
+                End If
+                If TextEdit1.Text = "Çok Hızlı" Then
+                    Timer1.Interval = 200
+                    Timer1.Start()
+                End If
+                If TextEdit1.Text = "Aşırı Hızlı" Then
+                    Timer1.Interval = 100
+                    Timer1.Start()
+                End If
+            End If
+        End If
+
+        If (e.KeyCode = Keys.F6) Then
+            Timer1.Stop()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.TopMost = True
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        SendKeys.Send(MemoEdit1.Text)
+        If CheckEdit1.Checked = True Then
+            SendKeys.Send("{ENTER}")
+        End If
+
+    End Sub
+End Class
